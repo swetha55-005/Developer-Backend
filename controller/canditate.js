a436810b95a9862ad7cb6259ad26d93b280cc01d
Saravanan <saravanan642@gmail.com>
2026-07-02 18:28:58 +0530

    update get and update

--- controller/canditate.js
+++ controller/canditate.js
@@ -4,6 +4,7 @@ const createCandidate = async (req, res) => {
     try {
         const allFields = req.body;
 
+
         if (Object.keys(allFields).length === 0) {
             return res.json({
                 success: false,
@@ -39,6 +40,49 @@ const createCandidate = async (req, res) => {
     }
 };
 
+
+const fetchCandidates = async (req ,res) => {
+    try{
+
+        const candidates = await CandidateModel.find({})
+
+        if(!candidates){
+           return res.json({success : "false" , message : "candidate not found"})
+        }
+
+        return res.json({success : false , message : "candidate fetch successfully" , data : candidates})
+
+    }
+    catch(err){
+        console.log("Error in fetch candidates")
+        return res.json({success : false , message : "Error in fetch candidates"})
+    }
+}
+
+const fetchCandidateByEmail = async (req ,res) => {
+      try{
+
+        const email = req.params
+
+        const candidate = await CandidateModel.findOne(email)
+
+        if(!candidate){
+           return res.json({success : "false" , message : "candidate not found"})
+        }
+
+        return res.json({success : false , message : "candidate fetch successfully" , data : candidate})
+
+    }
+    catch(err){
+        console.log("Error in fetch candidate")
+        return res.json({success : false , message : "Error in fetch candidate"})
+    }  
+}
+
+
+
 module.exports = {
-    createCandidate
+    createCandidate,
+    fetchCandidates,
+    fetchCandidateByEmail
 };
--- routes/candidate.js
+++ routes/candidate.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { createCandidate } = require("../controller/canditate");
+const { createCandidate ,fetchCandidates ,fetchCandidateByEmail} = require("../controller/canditate");
 
 router.post("/create-candidate", createCandidate);
+
+router.get('/fetch-candidates', fetchCandidates)
+
+router.get('/fetch-candidate/:email', fetchCandidateByEmail)
+
+
 
 module.exports = router;
