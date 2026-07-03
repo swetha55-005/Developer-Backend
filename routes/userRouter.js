06323f7eb3a414f7da4cf841e70193bb6e5dd87c
swetha55-055 <srswetha064@gmail.com>
2026-07-03 18:08:57 +0530

    new cahnges

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,5 @@
 const express = require('express')
-const { createUser , fetchUser } = require('../controller/user')
+const { createUser, fetchUser } = require('../controller/user')
 
-const router = express.Router()
-
-
-router.post('/create-user', createUser)
-router.get('/fetch-user', fetchUser)
-
-
-
-module.exports = router
+console.log("createUser:", createUser)
+console.log("fetchUser:", fetchUser)
